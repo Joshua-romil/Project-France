8346fa9d4abd329ea39160e7629b31d4875cb30a
Joshua George <joshuageorge@Joshuas-MacBook-Pro.local>
2018-07-30 23:02:08 +0200

    Init files

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Veloar',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>30th century Marketing</h1>
+    <p>We offer a wide services for your social media profile</p>
+    <p>Start optimzing your social media flow today</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
