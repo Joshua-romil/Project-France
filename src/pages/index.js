b4aab6e909c5ce1097e2dedac8e4646e3e833334
Joshua George <joshuageorge@Joshuas-MacBook-Pro.local>
2018-08-31 23:20:52 +0200

    Added header, added images

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>30th century Marketing</h1>
-    <p>We offer a wide services for your social media profile</p>
-    <p>Start optimzing your social media flow today</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>30th century Marketing</h1>
+        <p>We offer a wide services for your social media profile. Start optimzing your social media flow today</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </div>
 )
 
